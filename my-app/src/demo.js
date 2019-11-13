5bacf677a2b287c7c4c62c0e75829571f33f351d
jim <535258211@qq.com>
2019-11-13 17:39:54 +0800

    init

---
+++ my-app/src/demo.js
@@ -0,0 +1,81 @@
+import React,{Component} from 'react';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name:'react'
+        }
+    }
+    render (){
+        console.log("App render")
+        return (
+            <div>
+                {this.state.name}
+                {this.state.name && <Son name={this.state.name + '-son'}/>}
+            </div>
+
+        )
+    }
+    componentDidMount(){
+        window.app = this;
+        console.log("App mount")
+    }
+    componentWillUpdate(){
+        console.log("App Will Update")
+    }
+    componentDidUpdate(){
+        console.log("App did Update")
+    }
+    componentWillUnmount(){
+        console.log("App did UnMount")
+    }
+}
+
+class Son extends Component {
+    render (){
+        console.log("son render")
+        return (
+            <div>
+                {this.props.name}
+                <GrandSon name={this.props.name + '-GrandSon'}/>
+            </div>
+        )
+    }
+    componentDidMount(){
+        console.log("son mount")
+    }
+    componentWillUpdate(){
+        console.log("Son Will Update")
+    }
+    componentDidUpdate(){
+        console.log("Son did Update")
+    }
+    componentWillUnmount(){
+        console.log("Son did UnMount")
+    }
+}
+
+class GrandSon extends Component {
+    render (){
+        console.log("GrandSon render")
+        return (
+            <div>
+                {this.props.name}
+            </div>
+        )
+    }
+    componentDidMount(){
+        console.log("GrandSon mount")
+    }
+    componentWillUpdate(){
+        console.log("GrandSon Will Update")
+    }
+    componentDidUpdate(){
+        console.log("GrandSon did Update")
+    }
+    componentWillUnmount(){
+        console.log("GrandSon did UnMount")
+    }
+}
+export default App;
